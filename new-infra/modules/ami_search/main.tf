2fb8350f6f22262a7776f6c1cc8da453d249b9ad
José Flores <josefloressv@gmail.com>
2024-04-14 18:05:00 -0600

    AMI search module AL2023 by default

---
+++ new-infra/modules/ami_search/main.tf
@@ -0,0 +1,24 @@
+data "aws_ami" "selected" {
+  most_recent      = true
+  owners           = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = [var.ami_name_filter]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+}
---
+++ new-infra/modules/ami_search/variables.tf
@@ -0,0 +1,4 @@
+variable "ami_name_filter" {
+    type = string
+    default = "al2023-ami-2023.*"
+}
