247149ddcdfd38abcd2a0db0915d21f4dc842e15
José Flores <josefloressv@gmail.com>
2024-04-26 00:07:25 -0600

    Refactor test

--- k6/spike.js
+++ k6/spike.js
@@ -1,18 +1,25 @@
 import { sleep } from "k6"
 import http from "k6/http"
+
+const url = "http://demoapp-dev-327614256.us-east-1.elb.amazonaws.com/owners?lastName=";
+
 export const options = {
     stages: [
         {
-            target: 100,
+            target: 800,
             duration: "30s"
         },
         {
             target: 400,
-            duration: "30s" 
+            duration: "20s" 
+        },
+        {
+            target: 0,
+            duration: "10s" 
         }
     ],
 }
 export default function(){
-    let response = http.get("http://demoapp-dev-1621236630.us-east-1.elb.amazonaws.com")
+    let response = http.get(url)
     sleep(1)
 }
--- k6/stress.js
+++ k6/stress.js
@@ -1,26 +1,43 @@
 import { sleep } from "k6"
 import http from "k6/http"
+import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
+
+const url = 'http://demoapp-dev-327614256.us-east-1.elb.amazonaws.com/owners/new';
+
 export const options = {
     stages: [
         {
-            target: 100,
+            target: 800,
             duration: "1m"
         },
         {
-            target: 200,
+            target: 1200,
             duration: "1m" 
         },
         {
             target: 400,
+            duration: "1m" 
+        },
+        {
+            target: 2000,
             duration: "2m" 
         },
         {
             target: 0,
             duration: "1m" 
-        }
+        },
     ],
 }
 export default function(){
-    let response = http.get("http://demoapp-dev-1621236630.us-east-1.elb.amazonaws.com/owners?lastName=")
+    let formData = {
+        firstName: 'Rodrigo',
+        lastName: "Chamorro " + randomIntBetween(1, 1000),
+        address: 'San Salvador',
+        city: 'San Salvador',
+        telephone: '555555555'
+    };
+    let headers = { 'Content-Type': 'application/x-www-form-urlencoded' };
+    let response = http.post(url, formData, { headers: headers });
+    // console.log(response.status)
     sleep(1)
 }
